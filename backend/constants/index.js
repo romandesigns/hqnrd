6f9f578623aecdd12519ff7fdab7bcf33b0bb52d
creativecoder87 <romandesigns.us@gmail.com>
2021-01-10 15:33:43 -0500

    Created backend constant variables

---
+++ backend/constants/index.js
@@ -0,0 +1,15 @@
+// SERVER APP CONSTANTS
+export const APP_LISTEN_FEEDBACK = "App up in running in ";
+
+// CONTROLLERS CONSTANTS
+export const CONTENT_NOT_FOUND = "Contenido no encontrado";
+export const OBJECT_ID_NOT_VALID = "El contenido que buscas no es valido o encontrado";
+export const INTERNAL_PROBLEM_FOUND = "Error interno. Porfavor reporte incidente al soporte technico: romandesigns.us@gmail.com";
+export const ACCOUNT_NOT_CREATED = "Cuenta no creada, porfavor revise la infomacion proveida y intentelo nuevamente";
+export const ACCOUNT_UPDATED = "Informacion actualizada";
+export const ACCOUNT_NOT_UPDATED = "Porvafor verifique la informacion y vuelva a intentarlo";
+export const ACCOUNT_DELETED = "La cuenta a sido eliminada";
+
+// DB CONSTANTS
+export const DB_CONNECTED = "Connected to DB";
+export const DB_ERROR = "A problem has occurred in the database. Pleas try again later";
