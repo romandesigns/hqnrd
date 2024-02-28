cbe62fa0bb1452c7db1b76e4d4b2d6340a4222f8
Roman F <wavystyledev@gmail.com>
2024-02-28 09:30:07 -0500

    made public layout component to make jsx more manageable

--- app/[lang]/(public)/page.tsx
+++ app/[lang]/(public)/page.tsx
@@ -1,20 +1,26 @@
-import { FrequentlyAskedQuestions } from "../ui/layout/components";
-import { Amenities, Gallery } from "../ui/layout/components/Home";
+import { FrequentlyAskedQuestions, Header } from "../ui/layout/components";
+import { Amenities, Gallery, HomeHeader } from "../ui/layout/components/Home";
 import { Category } from "../ui/layout/components/Home/Category";
 import Discounts from "../ui/layout/components/public/Home/Discounts";
 import Testimonials from "../ui/layout/components/public/Home/Testimonials";
 import Trendings from "../ui/layout/components/public/Home/Trending";
+import { PublicLayout } from "../ui/layout/containers/PublicLayout";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-between px-2">
-      <Gallery />
-      <Testimonials />
-      <Amenities />
-      <Discounts />
-      <Category />
-      <Trendings />
-      <FrequentlyAskedQuestions />
-    </main>
+    <PublicLayout>
+      <Header className="h-screen flex items-stretch justify-stretch p-2 header-height">
+        <HomeHeader />
+      </Header>
+      <main className="flex flex-col items-center justify-between px-2">
+        <Gallery />
+        <Testimonials />
+        <Amenities />
+        <Discounts />
+        <Category />
+        <Trendings />
+        <FrequentlyAskedQuestions />
+      </main>
+    </PublicLayout>
   );
 }
