91937b1fb546ddb0b1411759a0e8330b81f99277
Roman F. <wavystyledev@gmail.com>
2025-02-07 11:37:38 -0500

    added heading to searchbox

--- components/features/page/home/ReservationSearchBox/index.tsx
+++ components/features/page/home/ReservationSearchBox/index.tsx
@@ -7,34 +7,39 @@ import { Locale } from "@/i18n-config";
 
 export function ReservationSearchBox({ lang }: { lang: Locale }) {
   return (
-    <Content className="max-w-5xl rounded-lg bg-primary-foreground p-5 shadow-2xl">
-      <article className="grid h-full w-full grid-cols-[1fr_1fr_1fr_1fr_10rem] grid-rows-1 items-center justify-center gap-5 rounded-lg p-5">
-        <InputNumber
-          name="adultsCount"
-          inputNumberLabel="Adults"
-          iconName="FaUser"
-          iconSize={13}
-        />
-        <InputNumber
-          name="childrensCount"
-          inputNumberLabel="Children"
-          iconName="FaChild"
-          iconSize={16}
-        />
-        <DateAndTimePicker
-          lang={lang}
-          hideIcon={true}
-          label="Check in date"
-          icon="calendar"
-        />
-        <DateAndTimePicker
-          lang={lang}
-          hideIcon={true}
-          label="Check out date"
-          icon="calendar"
-        />
-        <Button className="mt-6">Search</Button>
-      </article>
-    </Content>
+    <>
+      <Content className="max-w-5xl">
+        <h2 className="py-6 text-center text-2xl font-bold">
+          Start booking today!
+        </h2>
+        <article className="grid h-full w-full grid-cols-[1fr_1fr_1fr_1fr_10rem] grid-rows-1 items-center justify-center gap-5 rounded-lg bg-primary-foreground p-10 shadow-2xl">
+          <InputNumber
+            name="adultsCount"
+            inputNumberLabel="Adults"
+            iconName="FaUser"
+            iconSize={13}
+          />
+          <InputNumber
+            name="childrensCount"
+            inputNumberLabel="Children"
+            iconName="FaChild"
+            iconSize={16}
+          />
+          <DateAndTimePicker
+            lang={lang}
+            hideIcon={true}
+            label="Check in date"
+            icon="calendar"
+          />
+          <DateAndTimePicker
+            lang={lang}
+            hideIcon={true}
+            label="Check out date"
+            icon="calendar"
+          />
+          <Button className="mt-6">Search</Button>
+        </article>
+      </Content>
+    </>
   );
 }
