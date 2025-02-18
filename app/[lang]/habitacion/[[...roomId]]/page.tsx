d0de969319fb95884dc642541c665d74ef3d4fe6
Roman F. <wavystyledev@gmail.com>
2025-02-17 19:00:26 -0500

    clean up

--- app/[lang]/habitacion/[[...roomId]]/page.tsx
+++ app/[lang]/habitacion/[[...roomId]]/page.tsx
@@ -2,6 +2,6 @@ import { Locale } from "@/i18n-config";
 
 export default async function Page({params}:{params: Promise<{roomId:string, lang:Locale}>}) {
   const foo = await params;
-console.log(foo);
+
   return <p>Page ID: 44</p>;
 }
