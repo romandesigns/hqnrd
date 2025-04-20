5d28dd82f1d3aaa62e582f701a0e1d247f63adf7
Roman F. <wavystyledev@gmail.com>
2025-04-20 02:26:57 -0400

    hooked up convex with clerk

--- app/api/auth/webhooks/route.ts
+++ app/api/auth/webhooks/route.ts
@@ -1,7 +1,9 @@
 import { verifyWebhook } from "@clerk/nextjs/webhooks";
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
+import { fetchMutation } from "convex/nextjs";
+import { api } from "@/convex/_generated/api";
 
-export async function POST(req: NextRequest) {
+export async function POST(req: NextRequest, res: Response) {
   try {
     const evt = await verifyWebhook(req);
 
@@ -12,7 +14,55 @@ export async function POST(req: NextRequest) {
     console.log(
       `Received webhook with ID ${id} and event type of ${eventType}`,
     );
-    console.log("Webhook payload:", evt.data);
+    // console.log("Webhook payload:", evt.data);
+    switch (eventType) {
+      case "user.created":
+        // Handle user created event
+        try {
+          const payload = {
+            clerkId: evt.data.id,
+            email: evt.data.email_addresses[0].email_address,
+            profileImage: evt.data.image_url,
+            username: evt.data.username || "n/a",
+          };
+          await fetchMutation(api.users.createUser, payload);
+
+          return NextResponse.json({
+            status: 200,
+            message: "User info inserted",
+          });
+        } catch (error) {
+          return NextResponse.json({
+            status: 400,
+            error,
+          });
+        }
+        break;
+      case "user.updated":
+        // Handle user updated event
+        console.log("User updated:", evt.data);
+
+        const updatePayload = {
+          clerkId: evt.data.id,
+          email: evt.data.email_addresses[0].email_address,
+          profileImage: evt.data.image_url,
+          username: evt.data.username || "n/a",
+        };
+        // await fetchMutation(api.users.updateUser, updatePayload);
+        return NextResponse.json({
+          status: 200,
+          message: "User info updated",
+        });
+        break;
+      case "user.deleted":
+        // Handle user deleted event
+        console.log("User deleted:", evt.data);
+        break;
+      default:
+        console.log("Unhandled event type:", eventType);
+        break;
+    }
+    // You can also use the event type to determine what action to take
 
     return new Response("Webhook received", { status: 200 });
   } catch (err) {
--- convex/users.ts
+++ convex/users.ts
@@ -18,3 +18,21 @@ export const createUser = mutation({
     return user;
   },
 });
+
+// export const updateUser = mutation({
+//   args: {
+//     clerkId: v.string(),
+//     email: v.string(),
+//     profileImage: v.string(),
+//     username: v.string(),
+//   },
+//   handler: async (ctx, args) => {
+//     const user = await ctx.db.get("users", args.clerkId).update({
+//       email: args.email,
+//       profileImage: args.profileImage,
+//       username: args.username,
+//     });
+//     return user;
+//   },
+// });
+// //     return NextResponse.json({
